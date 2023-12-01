2d60f74aae999ab67c6a1d92762d682c2c6d65ff
captainAyan <ayanchakraborty105@gmail.com>
2023-12-01 14:28:13 +0530

    fixed 'movable' option on player

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -12,40 +12,34 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var last_character_face_direction = 1 # 1 is right and -1 is left
 
 func _process(delta):
-	if movable:
-		if velocity.x != 0:
-			$AnimationPlayer.play("walk_anim", -1, 5, false);
-		else:
-			$AnimationPlayer.play("idle_anim");
-		
-		# Add the gravity.
-		if not is_on_floor():
-			velocity.y += gravity * delta
+	if velocity.x != 0:
+		$AnimationPlayer.play("walk_anim", -1, 5, false);
+	else:
+		$AnimationPlayer.play("idle_anim");
 
-		# Handle Jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction = Input.get_axis("ui_left", "ui_right")
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-		# change character sprite direction
-	#	if direction != 0 && last_character_face_direction != direction:
-	#		last_character_face_direction = direction
-	#		$FullBody.scale.x = -$FullBody.scale.x # the flip
-		if direction > 0.5:
-			last_character_face_direction = 1
-			$FullBody.scale.x = 1
-		elif direction < -0.5:
-			last_character_face_direction = -1
-			$FullBody.scale.x = -1
-		else:
-			$FullBody.scale.x = last_character_face_direction
-	
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and movable:
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction and movable:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if direction > 0.5 and movable:
+		last_character_face_direction = 1
+		$FullBody.scale.x = 1
+	elif direction < -0.5 and movable:
+		last_character_face_direction = -1
+		$FullBody.scale.x = -1
+	else:
+		$FullBody.scale.x = last_character_face_direction
 
 	move_and_slide()
