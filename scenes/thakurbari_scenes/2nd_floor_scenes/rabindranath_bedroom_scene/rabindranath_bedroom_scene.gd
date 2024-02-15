0cf971a314c7c5044b97fa1ae1647ec04874e79d
captainAyan <ayanchakraborty105@gmail.com>
2024-02-16 01:07:06 +0530

    removed collision shape from trigger template, and added collision shape to trigger instance

--- scenes/thakurbari_scenes/2nd_floor_scenes/rabindranath_bedroom_scene/rabindranath_bedroom_scene.gd
+++ scenes/thakurbari_scenes/2nd_floor_scenes/rabindranath_bedroom_scene/rabindranath_bedroom_scene.gd
@@ -3,10 +3,10 @@ extends Node2D
 func _ready():
 	if StorylineTracker.get_current_state_name() == "wakes_up":
 		StorylineTracker.complete_objective("wakes_up")
-		$AnimationPlayer.play("wake_up_anim")
+#		$AnimationPlayer.play("wake_up_anim")
 		
-#		$AfterWakeUpDialog.start() # testing
-#		dialog_start_side_effect() # testing
+		$AfterWakeUpDialog.start() # testing
+		dialog_start_side_effect() # testing
 
 
 func _on_animation_player_animation_finished(anim_name):
